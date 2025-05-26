466fb81e192e3dc774f63bc9f5b679b76efa2cec
Abdul Ahad <abdulahad53688@gmail.com>
2025-05-26 17:10:05 +0500

    ahad/feat/Fixed all the reviews

--- word.py
+++ word.py
@@ -1,51 +1,70 @@
 import sys
 
-def display(
-    word_frequency_counts):
+def read_path_with_validation(input_path):
+    """Read the file and return if the file path is correct.
+
+    Args:
+        file_path : To check the path.
+    """
+    try:
+        with open(input_path, 'r') as fin:
+            return fin.read()
+    except FileNotFoundError:
+        print(f"Error: File '{input_path}' not found.")
+        sys.exit(1)
+
+def main(input_path):
+    """Recieves text to check validation path and to call word frequency function.
+
+    Args:
+        text (file_path).
+    """
+    text = read_path_with_validation(input_path)
+    word_frequency_counter(text)
+
+
+def display(word_frequency_counts):
     """Display word frequencies in a formatted table.
 
     Args:
         word_frequency_counts (list): List of tuples containing (word, frequency).
     """
-    print("Word                 Frequency")
-    print("-------------------------------")
+    print('Word                 Frequency')
+    print('-------------------------------')
     for word, frequency in word_frequency_counts:
-        print(f"{word:<25} {frequency}")
-    print("-------------------------------")
+        print(f'{word:<25} {frequency}')
+    print('-------------------------------')
         
-def descending_order(
-    word_frequency_counts):
+def descending_order(word_frequency_counts):
     """Changing the orignal order into Descending order.
 
     Args:
         word_frequency_counts (list): List of tuples containing (word, frequency).
     """
-    for i in range (len(word_frequency_counts)):
-        for j in range(i,len(word_frequency_counts)-1):
-            if word_frequency_counts[i][1]<word_frequency_counts[j][1]:
-                temp=word_frequency_counts[i]
-                word_frequency_counts[i]=word_frequency_counts[j]
-                word_frequency_counts[j]=temp
+    for current in range (len(word_frequency_counts)):
+        for next in range(current,len(word_frequency_counts) - 1):
+            if word_frequency_counts[current][1] < word_frequency_counts[next][1]:
+                word_frequency_counts[current], word_frequency_counts[next] = word_frequency_counts[next], word_frequency_counts[current]
+
     display(word_frequency_counts)
         
         
-def word_frequency_counter(
-    text):
+def word_frequency_counter(text):
     """Creating unique word list and word_frequency_count.
 
     Args:
         Text (list): List of input text.
     """
-    words_list=text.split()
-    word_frequency_counts=[]
-    unique_word=[]
+    words_list = text.split()
+    word_frequency_counts = []
+    unique_word = []
     for match in words_list:
         if match not in unique_word:
-            counter=0
+            counter = 0
             unique_word.append(match)
             for other in words_list:
-                if match==other :
-                    counter+=1
+                if match == other :
+                    counter += 1
             word_frequency_counts.append((match,counter))
     
     descending_order(word_frequency_counts)
@@ -57,19 +76,12 @@ def word_frequency_counter(
 if __name__ == "__main__":
     
     
-    if len(sys.argv)!=2:
-        print("Invalid Arguments!!!")
+    if len(sys.argv) != 2:
+        print('Invalid Arguments!!!')
         sys.exit(1)
 
-    input_file_path=sys.argv[1]
-    
-    try:
-        with open(input_file_path,'r')as file:
-            text=file.read()
-        word_frequency_counter(text)
-    except FileNotFoundError:
-        print(f"Error: File '{input_file_path}' not found.")
-        sys.exit(1) 
+    main(sys.argv[1])
+
 
     
 
