f9a99319762e637f9121b16f27815da3ca89f0c1
Abdul Ahad <abdulahad53688@gmail.com>
2025-05-29 09:42:10 +0500

    Improving code quality using libraries.

--- word.py
+++ word.py
@@ -1,9 +1,10 @@
 import sys
 import re
 from collections import Counter
+import argparse 
 
 
-def read_path_with_validation(input_path):
+def read_input_with_path_validation(input_path):
     """Read the file and return if the file path is correct.
 
     Args:
@@ -16,19 +17,20 @@ def read_path_with_validation(input_path):
         print(f"Error: File '{input_path}' not found.")
         sys.exit(1)
 
+
 def main(input_path):
-    """Recieves input_file_path to check validation path and to call word frequency function.
+    """Receives input_file_path to check validation path and to call word frequency function.
 
     Args:
         text (file_path).
     """
-    content = read_path_with_validation(input_path)
+    content = read_input_with_path_validation(input_path)
     count_word_frequency(content)
 
 
 def display(word_frequency):
     """Display word frequencies in a formatted table.
-    
+
     Args:
         word_frequency_counts (list): List of tuples containing (word, frequency).
     """
@@ -37,32 +39,33 @@ def display(word_frequency):
     for word, frequency in word_frequency:
         print(f'{word:<25} {frequency}')
     print('-------------------------------')
-        
-def ort_by_frequency_desc(word_frequency):
-    """Changing the orignal order into Descending order.
+
+
+def sort_by_frequency_desc(word_frequency):
+    """Changing the original order into Descending order.
 
     Args:
         word_frequency_counts (list): List of tuples containing (word, frequency).
     """
     word_frequency.sort(key=lambda x: x[1], reverse=True)
-
+    
     display(word_frequency)
-        
-        
+
+
 def count_word_frequency(content):
     """It computes the unique word frequency.
 
     Args:
         Text (string): string of input text.
     """
-    words = re.findall(r'\b\w+\b',content)
-    word_frequency= list(Counter(words).items())
-    ort_by_frequency_desc(word_frequency)
-        
-if __name__ == "__main__":    
-    if len(sys.argv) != 2:
-        print('Invalid Arguments!!!')
-        sys.exit(1)
+    words = re.findall(r'\b\w+\b', content)
+    word_frequency = list(Counter(words).items())
+    sort_by_frequency_desc(word_frequency)
 
-    main(sys.argv[1])
-    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Word Frequency Counter')
+    parser.add_argument('input_file', type=str, help='Path to input text file')
+    args = parser.parse_args()
+
+    main(args.input_file)
